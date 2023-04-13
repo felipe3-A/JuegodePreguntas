0e234962bb4506eea7e581ea3f8352538604628a
felipe3-A <lenovo.esucre@gmail.com>
2023-04-13 18:54:04 -0500

    Vista de la pregunt2

--- app/src/main/java/com/felipe/juegodepreguntas/MainActivity.java
+++ app/src/main/java/com/felipe/juegodepreguntas/MainActivity.java
@@ -2,9 +2,11 @@ package com.felipe.juegodepreguntas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +32,30 @@ public class MainActivity extends AppCompatActivity {
         pre1_res2=findViewById(R.id.res2);
         pre1_res3=findViewById(R.id.res3);
         pre1_res4=findViewById(R.id.res4);
+
+        //Activar evento
+
+        grupo1pregunta1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    //El toates un tipo de alert para movil en este caso avisa si la casilla marcada es coorecta o no
+                    case R.id.res1:
+                        Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
+                    case R.id.res2:
+                        Toast.makeText(MainActivity.this, "¡Correcto!", Toast.LENGTH_SHORT).show();
+                        //Si seleciona esta respuesta lo dirige la siguiente Vista
+                        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                        startActivity(intent);
+
+                    case R.id.res3:
+                        Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
+                    case R.id.res4:
+                        Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/felipe/juegodepreguntas/MainActivity2.java
@@ -0,0 +1,56 @@
+package com.felipe.juegodepreguntas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+   //Referenciar vista2
+    RadioGroup grupo2;
+    RadioButton op1;
+    RadioButton op2;
+    RadioButton op3;
+    RadioButton op4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        //Metodo
+        referenciar();
+    }
+
+    private void referenciar() {
+        grupo2=findViewById(R.id.grupopregunta2);
+        op1=findViewById(R.id.res12);
+        op2=findViewById(R.id.res22);
+        op3=findViewById(R.id.res32);
+        op4=findViewById(R.id.res42);
+
+        //Activar Evento
+
+        grupo2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int k) {
+                switch (k){
+                    case R.id.res12:
+                        Toast.makeText(MainActivity2.this, "Correcto x2", Toast.LENGTH_SHORT).show();
+
+                    case R.id.res22:
+                        Toast.makeText(MainActivity2.this, "InCorrecto", Toast.LENGTH_SHORT).show();
+
+                    case R.id.res32:
+                        Toast.makeText(MainActivity2.this, "InCorrecto", Toast.LENGTH_SHORT).show();
+
+                    case R.id.res42:
+                        Toast.makeText(MainActivity2.this, "InCorrecto", Toast.LENGTH_SHORT).show();
+
+                        break;
+                }
+            }
+        });
+    }
+}
